4edc996caf06661d341d2c42925c0056aa8535a2
Grimes <crameg@amazon.com>
2019-08-06 17:24:49 -0400

    add paragraphs to other elements

--- hello.js
+++ hello.js
@@ -1,3 +1,16 @@
-function sayHello(myNumber) {
-    alert("Hello! " + myNumber);
+function addPara(thInput) {
+    console.log('hit');
+    if (thInput.length > 0)
+    {
+    console.log('in if');
+    var para = document.createElement("p");
+    var node = document.createTextNode("! " + thInput + " !");
+    para.appendChild(node);
+
+
+    var element = document.getElementById("theoutput");
+    element.appendChild(para);
+
+
+    }
 }
