e6c21764668bc9295abeda424e23fce93ec0f1d3
bluerosinneo <cramergrimes@gmail.com>
2019-08-14 20:37:24 -0400

    don't forget to use branches

--- hello.js
+++ hello.js
@@ -1,8 +1,6 @@
 function addPara(thInput) {
-    console.log('hit');
     if (thInput.length > 0)
     {
-    console.log('in if');
     var para = document.createElement("p");
     var node = document.createTextNode("! " + thInput + " !");
     para.appendChild(node);
